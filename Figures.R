1bfcf6c8bf21a0e376de13cc1f08880f7147ecc1
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-02-10 18:21:37 -0700

    Update Figures.R
    Added code to draw map of Nevada with boxes, some changes to existing functions.

--- Figures.R
+++ Figures.R
@@ -9,6 +9,7 @@ library(rnaturalearthhires)
 library(leaflet)
 library(webshot2)
 library(dplyr)
+library(mapview)
 
 
 # create subdirectory to store figure outputs
@@ -114,6 +115,52 @@ Box3Data <- read.csv("Data/BoxThreeData.csv")
 Box4Data <- read.csv("Data/BoxFourData.csv")
 
 
+# define function for drawing bounding box
+find_bbox <- function(map_data) {
+  
+  # Remove rows where a specific column has NA values
+  map_data <- map_data %>%
+    filter(!is.na(lat) & !is.na(long))
+ 
+  # check if coordinates are the same
+  all_same <- all(map_data$long == map_data$long[1])
+  
+  if (all_same) {
+    xmin = mean(map_data$long) - 0.5
+    xmax = mean(map_data$long) + 0.5
+    ymin = mean(map_data$lat) - 0.3
+    ymax = mean(map_data$lat) + 0.3
+    
+  } else {
+    # find original bounding box
+    coordinates = st_as_sf(map_data, coords = c("long", "lat"), crs = 4326)
+    bbox = st_bbox(coordinates)
+    
+    # set margin to 0.1 and find original height and width
+    margin = 0.1
+    width = bbox$xmax - bbox$xmin
+    height = bbox$ymax - bbox$ymin
+    
+    # adjust coordinates to accommodate margin
+    xmin = bbox$xmin - width * margin
+    xmax = bbox$xmax + width * margin
+    ymin = bbox$ymin - height * margin
+    ymax = bbox$ymax + height * margin
+  }
+  
+  # create new bounding box and convert so it can be recognized by ggplot2
+  bbox_coords = matrix(c(xmin, ymin,  # lower-left
+                         xmax, ymin,  # lower-right
+                         xmax, ymax,  # upper-right
+                         xmin, ymax,  # upper-left
+                         xmin, ymin), # close the polygon
+                       ncol = 2, byrow = TRUE)
+  bbox_polygon = st_polygon(list(bbox_coords))
+  bbox_sf = st_sfc(bbox_polygon, crs = 4326)
+  expanded_bbox <- st_bbox(bbox_sf)
+  
+  return(expanded_bbox)}
+
 # define function for drawing each box
 smallBox <- function(mapdata_name) {
   
@@ -123,30 +170,7 @@ smallBox <- function(mapdata_name) {
   # Remove rows where a specific column has NA values
   mapdata <- mapdata %>%
     filter(!is.na(lat) & !is.na(long))
-  
-  # define function for drawing bounding box
-  find_bbox <- function(map_data) {
-    
-    # adjust coordinates to accommodate margin
-    xmin = mean(map_data$long) - 1
-    xmax = mean(map_data$long) + 1
-    ymin = mean(map_data$lat) - 0.7
-    ymax = mean(map_data$lat) + 0.7
-    
-    # create new bounding box and convert so it can be recognized by ggplot2
-    bbox_coords = matrix(c(xmin, ymin,  # lower-left
-                           xmax, ymin,  # lower-right
-                           xmax, ymax,  # upper-right
-                           xmin, ymax,  # upper-left
-                           xmin, ymin), # close the polygon
-                         ncol = 2, byrow = TRUE)
-    bbox_polygon = st_polygon(list(bbox_coords))
-    bbox_sf = st_sfc(bbox_polygon, crs = 4326)
-    expanded_bbox <- st_bbox(bbox_sf)
-    
-    return(expanded_bbox)}
-  
-  
+
   # call function to find bbox and further modify it so it can be placed on map
   bbox <- find_bbox(mapdata)
   bbox_sfc <- st_as_sfc(bbox)
@@ -175,3 +199,28 @@ smallBox(mapdata_name = "Box1Data")
 smallBox(mapdata_name = "Box2Data")
 smallBox(mapdata_name = "Box3Data")
 smallBox(mapdata_name = "Box4Data")
+
+# create function to draw state map with boxes on it
+
+# ensure any overlays are properly saved
+mapviewOptions(fgb = FALSE) 
+
+# Get US states and filter for Nevada
+us_states <- ne_states(country = "United States of America", returnclass = "sf")
+nevada <- us_states[us_states$name == "Nevada", ]
+
+# call function to find bbox and further modify it so it can be placed on map
+bbox1 <- st_as_sfc(find_bbox(Box1Data))
+bbox2 <- st_as_sfc(find_bbox(Box2Data))
+bbox3 <- st_as_sfc(find_bbox(Box3Data))
+bbox4 <- st_as_sfc(find_bbox(Box4Data))
+
+# Map Nevada
+mapNevada <- mapview(nevada, legend = FALSE) +
+                mapview(bbox1, color = "black", fill = FALSE, alpha = 1, lwd = 3) +
+                mapview(bbox2, color = "black", fill = FALSE, alpha = 1, lwd = 3) +
+                mapview(bbox3, color = "black", fill = FALSE, alpha = 1, lwd = 3) +
+                mapview(bbox4, color = "black", fill = FALSE, alpha = 1, lwd = 3)
+
+# Save as PNG with higher resolution
+mapshot(mapNevada, file = "FiguresResults/NevadaMap.png", vwidth = 2000, vheight = 1500)
