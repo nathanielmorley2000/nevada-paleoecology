73eaea6eb270e67c21076d403f841021b3f2fec3
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-02-14 15:12:48 -0700

    Update Protocol1.R
    Cleaned up existing code. Added code to loop through and calculate success ratio for 1.00-0.05, and create/export a summary dataframe and plot.

--- Protocol1.R
+++ Protocol1.R
@@ -3,33 +3,36 @@ library("dplyr")
 library("vegan")
 library("ggplot2")
 
-# overhead for creating Results subdirectory
-current_time <- Sys.time()
-formatted_time <- format(current_time, "%Y-%m-%d_%H-%M-%S")
-folder_name <- paste("Results", formatted_time, sep = " ")
-dir.create(folder_name)
+# create directory to store protocol 1 results
+dir.create("Protocol1Results")
 
 # load data and replace NAs with 0s
 rawData <- read.csv("Data/RelativeDataset.csv")
 data <- rawData %>% replace(is.na(.), 0) # ensure NA values are replaced with 0
 
+# create function for running u-test on NMDS axis 1 scores and returning the U-statistic and p-value
 mann_whitney_test <- function(NMDS1, Early.Mid, data.scores) {
-  test <- wilcox.test(NMDS1 ~ Early.Mid, data = data.scores)  # Use exact = FALSE for larger samples
+  test <- wilcox.test(NMDS1 ~ Early.Mid, data = data.scores)
   U_stat <- test$statistic
   p_value <- test$p.value
   return(c(U_stat, p_value))
 }
 
-success = 0
-
 # create function for looping NMDS and producing plots
-findNMDS <- function(r, nit, plotIndices) {
+findNMDS <- function(r, nit, save, plotIndices) {
   
-  # Initialize matrices to store U-statistics and p-values
-  results <- matrix(NA, nrow = nit, ncol = 1)
-  colnames(results) <- c("Success/Fail")
+  # set successes to 0 to calculate success ratio
+  success = 0
   
-  # Loop to perform the test nit times
+  # if you want to save individual results
+  if (save == TRUE){
+    
+    # overhead for creating individual results subdirectory
+    folder_name <- paste("Protocol1Results/IndividualResults", 1-r)
+    dir.create(folder_name)
+  }
+  
+  # loop to perform the test nit times
   i = 1
   for (i in 1:nit) {
     
@@ -40,110 +43,114 @@ findNMDS <- function(r, nit, plotIndices) {
     # CHECK 1: Make sure there are sufficient data (n = 5) in each sample to run U-test
     count_early = sum(culled_data$Early.Mid == "Early")
     count_mid = sum(culled_data$Early.Mid == "Middle")
+      
+    # make community matrix - extract columns with relative abundance information
+    com = culled_data[,4:ncol(culled_data)]
+    m_com = as.matrix(com)
     
-    # if successful, run NMDS
-    #if (count_early > 5 && count_mid > 5) {
-      
-      # make community matrix - extract columns with relative abundance information
-      com = culled_data[,4:ncol(culled_data)]
-      m_com = as.matrix(com)
-      
-      # run NMDS
-      nmds = metaMDS(m_com, distance = "bray")
-      
-      # extract NMDS scores (x and y coordinates) and bind to Early/Mid
-      data.scores = as.data.frame(scores(nmds)$sites)
-      data.scores$Early.Mid = culled_data$Early.Mid
-      
-      #data.scores <- read.csv("Results 2025-02-11_11-25-17/NMDSdata1.csv")
-    #}
+    # run NMDS
+    nmds = metaMDS(m_com, distance = "bray")
+    
+    # extract NMDS scores (x and y coordinates) and bind to Early/Mid
+    data.scores = as.data.frame(scores(nmds)$sites)
+    data.scores$Early.Mid = culled_data$Early.Mid
       
     # CHECK 2: Make sure NMDS didn't return a 1-D or mostly 1-D solution
     z_scores = (data.scores$NMDS1 - mean(data.scores$NMDS1)) / sd(data.scores$NMDS1)
     outliers = abs(z_scores) > 3 # check for outliers outside 3 standard deviations
-    
-    # if solution is not 1-dimensional, conduct U-test
-    #if (sum(outliers) != 1) {
-      # make sure "Early" is on the left of the plot
-      group_means <- aggregate(NMDS1 ~ Early.Mid, data = data.scores, mean)
-      earlyMean <- group_means$NMDS1[group_means$Early.Mid == "Early"]
-      midMean <- group_means$NMDS1[group_means$Early.Mid == "Middle"]
-      if (earlyMean > midMean) {
-        data.scores$NMDS1 <- -data.scores$NMDS1  # Flip the NMDS1 axis
-      }
       
+    # make sure "Early" is on the left of the plot
+    group_means <- aggregate(NMDS1 ~ Early.Mid, data = data.scores, mean)
+    earlyMean <- group_means$NMDS1[group_means$Early.Mid == "Early"]
+    midMean <- group_means$NMDS1[group_means$Early.Mid == "Middle"]
+    
+    # flip the NMDS1 axis if "early" is not on left
+    if (earlyMean > midMean) {
+      data.scores$NMDS1 <- -data.scores$NMDS1 
+    }
+    
+    # create plot and save axes scores if i < plotIndices 
+    if (i <= plotIndices) {
+      
+      # create function to calculate the convex hull for each group
+      find_hull = function(df) df[chull(df$NMDS1, df$NMDS2), ]
+      
+      # apply the function to your NMDS scores grouped by your factor
+      hulls = data.scores %>%
+        group_by(Early.Mid) %>%
+        do(find_hull(.))
+      
+      # NMDS plot with convex hulls
+      p = ggplot() +
+        geom_polygon(data = hulls, aes(x = NMDS1, y = NMDS2, fill = Early.Mid), alpha = 0.2) + # Convex hulls
+        geom_point(data = data.scores, aes(x = NMDS1, y = NMDS2, color = Early.Mid), size = 4) + # Points
+        theme_minimal() +
+        labs(x = "NMDS1", y = "NMDS2") +
+        ggtitle(paste("Plot for iteration", i)) +
+        theme(plot.title = element_text(hjust = 0.5))
+      
+      # construct the filename
+      file_name = sprintf("NMDS.%d.png", i)
+      file_path = file.path(folder_name, file_name)
+
+      # save the plot
+      ggsave(filename = file_path, plot = p, bg = "white")
+      
+      # save axis scores
       write.csv(data.scores, paste0(folder_name, "/NMDSdata", i, ".csv"))
-      
-      # create plot using ggplot2 if iteration is less than or equal to plotIndices
-      #if (i <= plotIndices) {
-        # Function to calculate the convex hull for each group
-        find_hull = function(df) df[chull(df$NMDS1, df$NMDS2), ]
-        
-        # Apply the function to your NMDS scores grouped by your factor
-        hulls = data.scores %>%
-          group_by(Early.Mid) %>%
-          do(find_hull(.))
-        
-        # NMDS plot with convex hulls
-        p = ggplot() +
-          geom_polygon(data = hulls, aes(x = NMDS1, y = NMDS2, fill = Early.Mid), alpha = 0.2) + # Convex hulls
-          geom_point(data = data.scores, aes(x = NMDS1, y = NMDS2, color = Early.Mid), size = 4) + # Points
-          theme_minimal() +
-          labs(x = "NMDS1", y = "NMDS2") +
-          ggtitle(paste("Plot for iteration", i)) +
-          theme(plot.title = element_text(hjust = 0.5))
-        
-        # Construct the filename
-        file_name = sprintf("NMDS.%d.png", i)
-        file_path = file.path(folder_name, file_name)
-        
-        # Save the plot
-        ggsave(filename = file_path, plot = p, bg = "white")
-        
-        u_test = mann_whitney_test(NMDS1, Early.Mid, data.scores)
-    #}
+    }
 
-      
-        if (all(count_early >= 5 && count_mid >= 5, sum(outliers) != 1, u_test[2] < 0.05)) {
-          print("success")
-          results[i,] = "success"
-          success = success + 1
-          
-        } else {
-          print("fail")
-          results[i,] = "fail"
-        }
-        
-        i = i + 1
-  }
+    # CHECK 3: Make sure the p-value of the u-test is significant (p < 0.05)
+    u_test = mann_whitney_test(NMDS1, Early.Mid, data.scores)
+    
+    # check that all 3 criteria are satisfied and mark as success
+    if (all(count_early >= 5 && count_mid >= 5, sum(outliers) != 1, u_test[2] < 0.05)) {
+      success = success + 1
+    }
+    
+    # proceed to next iteration
+    i = i + 1
+    }
   
-return(success)
+  # check that all iterations were performed correctly
+  if (i == nit + 1) {
+    
+    # calculate success ratio
+    successRatio = success/nit
+    return(successRatio)
+ 
+  } else {
+    print("Failure")
+  }
 }
-    
-# call function
-results <- findNMDS(r = 0.1, # proportion of data not culled
-                    nit = 80, # number of iterations
-                    plotIndices = 20) # number of plots produced
-results
-    
-write.csv(results, paste0(folder_name, "/success.csv"))    
-      
-# save individual results to the "Results" directory
-file_name <- "individual.iterations.csv"
-file_path <- file.path(folder_name, file_name)
-write.csv(results, file = file_path, row.names = TRUE)
 
-# Calculate the average U-statistic and p-value
-average_U_stat <- mean(results[, "U_stat"])
-average_p_value <- mean(results[, "p_value"])
+# define sequence and find number of rows
+r_values <- seq(0.05, 1.00, by = 0.05)
+n_rows <- length(r_values)
 
-# create matrix from summary results
-names_vector <- c("Proportion of Data Remaining", "Number of Iterations", "Proportion of Plots",
-                  "Average U-statistic", "Average p-value")
-values_vector <- c(r, nit, plotIndices, average_U_stat, average_p_value)
-matrix_data <- cbind(names_vector, values_vector)
+# initialize matrix
+results_matrix <- matrix(NA, nrow = n_rows, ncol = 2)
+results_matrix[, 1] <- 1 - r_values
+colnames(results_matrix) <- c("Proportion of Data Culled", "Success Rate")
 
-# save summary results to the "Results" directory
-file_name <- "summary.results.csv"
-file_path <- file.path(folder_name, file_name)
-write.csv(matrix_data, file = file_path, row.names = FALSE)
+# loop to compute values and store in the second column
+for (j in 1:n_rows) {
+  results_matrix[j, 2] <- findNMDS(r = r_values[j], # proportion of data retained (loop through at 0.05 increments)
+                                   nit = 5, # number of iterations
+                                   save = FALSE, # if you want to save individual plots and axis scores
+                                   plotIndices = 0) # number of plots produced (make sure save = TRUE)
+}
+
+# make matrix dataframe
+results_dataframe <- data.frame(results_matrix)
+
+# save matrix to Protocol1Results folder
+write.csv(results_dataframe, "Protocol1Results/Summary.csv", row.names = FALSE)
+
+# create a line plot
+p <- ggplot(results_dataframe, aes(x = Proportion.of.Data.Culled, y = Success.Rate)) +
+        geom_line(color = "black", size = 1) +
+        labs(x = "Proportion of Data Culled", y = "Success Rate") +
+        theme_classic()
+
+ggsave("Protocol1Results/SummaryPlot.png", p, width = 3543, height = 2657, units = "px")
